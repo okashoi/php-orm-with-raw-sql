a040a825cb7f24c0d594c158cbe5b16a2ce5237d
Okada Shohei <okashoicircus0@gmail.com>
2023-10-29 12:35:50 +0900

    implement ListUsersAction

--- webapp/src/User/ListUsersAction.php
+++ webapp/src/User/ListUsersAction.php
@@ -4,19 +4,29 @@ declare(strict_types=1);
 
 namespace App\User;
 
+use PDO;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class ListUsersAction
 {
+    public function __construct(
+        private PDO $db,
+    ) {
+    }
+
     public function __invoke(Request $request, Response $response): Response
     {
-        // TODO: DB の users テーブルから取得する
-        $users = [
-            new User(id: 1, name: 'Alice', createdAt: new \DatetImeImmutable('2023-10-01 9:00:00')),
-            new User(id: 2, name: 'Bob', createdAt: new \DatetImeImmutable('2023-10-15 12:00:00')),
-            new User(id: 3, name: 'Carol', createdAt: new \DatetImeImmutable('2023-10-31 15:00:00')),
-        ];
+        $this->db->beginTransaction();
+
+        $stmt = $this->db->query('SELECT * FROM users;');
+        /** @var list<User> $users */
+        $users = [];
+        while (($row = $stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
+            $users[] = User::fromRow($row);
+        }
+
+        $this->db->commit();
 
         $response->getBody()->write(json_encode($users, JSON_PRETTY_PRINT));
 
