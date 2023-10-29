090ada41fb9e861e1a8e341af765383c5baba254
Okada Shohei <okashoicircus0@gmail.com>
2023-10-29 15:25:34 +0900

    implement ListUsersAction

--- webapp/src/User/ListUsersAction.php
+++ webapp/src/User/ListUsersAction.php
@@ -4,19 +4,30 @@ declare(strict_types=1);
 
 namespace App\User;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class ListUsersAction
 {
+    public function __construct(
+        private EntityManager $db,
+    ) {
+    }
+
     public function __invoke(Request $request, Response $response): Response
     {
-        // TODO: DB の users テーブルから取得する
-        $users = [
-            new User(id: 1, name: 'Alice', createdAt: new \DatetImeImmutable('2023-10-01 9:00:00')),
-            new User(id: 2, name: 'Bob', createdAt: new \DatetImeImmutable('2023-10-15 12:00:00')),
-            new User(id: 3, name: 'Carol', createdAt: new \DatetImeImmutable('2023-10-31 15:00:00')),
-        ];
+        $rsm = new ResultSetMappingBuilder($this->db);
+        $rsm->addRootEntityFromClassMetadata(User::class, 'users');
+
+        $this->db->beginTransaction();
+
+        $query = $this->db->createNativeQuery('SELECT * FROM users', $rsm);
+        /** @var list<User> $users */
+        $users = $query->getResult();
+
+        $this->db->commit();
 
         $response->getBody()->write(json_encode($users, JSON_PRETTY_PRINT));
 
